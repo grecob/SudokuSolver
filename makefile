140dba97dec21fdaf811d1c0ade1e80bffb4821f
grecob <bryson.greco@gmail.com>
2022-05-11 23:47:42 -0500

    default constructer creates an empty board

--- makefile
+++ makefile
@@ -5,6 +5,9 @@ test-1-board: test/test-1-board.cpp board.o test/catch/catch.o
 	$(CXX) $(CXXFLAGS) -o test/$@ $^
 	test/$@ --success
 
+main: board.o main.o
+	$(CXX) $(CXXFLAGS) -o $@ $^
+
 clean:
 	rm -rf *.dSYM
 	$(RM) *.o *.gc test/test-1-board
--- test/test-1-board.cpp
+++ test/test-1-board.cpp
@@ -5,89 +5,89 @@
 TEST_CASE("Board")
 {
     Board defaultBoard;
-    std::vector<std::vector<int>> emptyBoard 
+    std::vector<std::vector<int>> easyBoard
     {
-        {0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0}
+        {0,5,0,7,6,0,0,0,3},
+        {0,0,8,0,0,9,1,0,0},
+        {3,0,0,0,0,4,9,6,2},
+        {0,9,4,3,0,5,0,0,6},
+        {8,0,7,0,2,0,0,4,0},
+        {2,0,0,4,1,0,3,0,8},
+        {0,0,6,0,0,0,0,7,0},
+        {4,1,0,0,0,2,0,0,5},
+        {0,2,0,5,9,8,0,1,4}
     };
-    Board paramBoard(emptyBoard);
+    Board paramBoard(easyBoard);
 
     SECTION("Test Constructors")
     {
-        CHECK(defaultBoard.GetBoard().empty() == false);
+        CHECK(paramBoard.GetBoard().empty() == false);
         // test GetData
-        CHECK(defaultBoard.GetData(0,1) == 5);
-        CHECK(defaultBoard.GetData(0,8) == 3);
-        CHECK(defaultBoard.GetData(8,5) == 8);
-        CHECK( paramBoard.GetBoard().empty() == false);
+        CHECK(paramBoard.GetData(0,1) == 5);
+        CHECK(paramBoard.GetData(0,8) == 3);
+        CHECK(paramBoard.GetData(8,5) == 8);
+        CHECK(paramBoard.GetBoard().empty() == false);
     }
 
     SECTION("setting data and out of bounds assignment")
     {
         // test SetData
-        paramBoard.SetData(0,0,2);
-        paramBoard.SetData(0,1,3);
-        CHECK(paramBoard.GetData(0,0) == 2);
-        CHECK(paramBoard.GetData(0,1) == 3);
+        defaultBoard.SetData(0,0,2);
+        defaultBoard.SetData(0,1,3);
+        CHECK(defaultBoard.GetData(0,0) == 2);
+        CHECK(defaultBoard.GetData(0,1) == 3);
         // test out of bounds assignment
-        paramBoard.SetData(2,2, -9);
-        CHECK(paramBoard.GetData(2,2) == 0);
+        defaultBoard.SetData(2,2, -9);
+        CHECK(defaultBoard.GetData(2,2) == 0);
     }
 
     SECTION("Check Rows and Columns")
     {
         // test CheckRow
-        CHECK(defaultBoard.CheckRow(0,5) == false);
-        CHECK(defaultBoard.CheckRow(0, 2) == true);
+        CHECK(paramBoard.CheckRow(0,5) == false);
+        CHECK(paramBoard.CheckRow(0, 2) == true);
         // test CheckColumn
-        CHECK(defaultBoard.CheckCol(0,8) == false);
-        CHECK(defaultBoard.CheckCol(0,1) == true);
+        CHECK(paramBoard.CheckCol(0,8) == false);
+        CHECK(paramBoard.CheckCol(0,1) == true);
     }
 
     SECTION("Test SubBoard Checks")
     {
-        CHECK(defaultBoard.CheckSubBoard(0,0,1) == true);
-        CHECK(defaultBoard.CheckSubBoard(0,0,2) == true);
-        CHECK(defaultBoard.CheckSubBoard(0,0,9) == true);
-        CHECK(defaultBoard.CheckSubBoard(0,0,5) == false);
-        CHECK(defaultBoard.CheckSubBoard(0,0,8) == false);
-        CHECK(defaultBoard.CheckSubBoard(0,0,3) == false);
+        CHECK(paramBoard.CheckSubBoard(0,0,1) == true);
+        CHECK(paramBoard.CheckSubBoard(0,0,2) == true);
+        CHECK(paramBoard.CheckSubBoard(0,0,9) == true);
+        CHECK(paramBoard.CheckSubBoard(0,0,5) == false);
+        CHECK(paramBoard.CheckSubBoard(0,0,8) == false);
+        CHECK(paramBoard.CheckSubBoard(0,0,3) == false);
 
-        CHECK(defaultBoard.CheckSubBoard(4,1,3) == true);
-        CHECK(defaultBoard.CheckSubBoard(4,1,8) == false);
+        CHECK(paramBoard.CheckSubBoard(4,1,3) == true);
+        CHECK(paramBoard.CheckSubBoard(4,1,8) == false);
 
-        CHECK(defaultBoard.CheckSubBoard(6,4,1) == true);
-        CHECK(defaultBoard.CheckSubBoard(6,4,2) == false);
+        CHECK(paramBoard.CheckSubBoard(6,4,1) == true);
+        CHECK(paramBoard.CheckSubBoard(6,4,2) == false);
     }
 
     SECTION("IsValid Checks")
     {
-        CHECK(defaultBoard.IsValid(0,0,1) == true);
-        CHECK(defaultBoard.IsValid(0,0,2) == false);
-        CHECK(defaultBoard.IsValid(0,0,9) == true);
-        CHECK(defaultBoard.IsValid(0,0,5) == false);
-        CHECK(defaultBoard.IsValid(0,0,8) == false);
-        CHECK(defaultBoard.IsValid(0,0,3) == false);
+        CHECK(paramBoard.IsValid(0,0,1) == true);
+        CHECK(paramBoard.IsValid(0,0,2) == false);
+        CHECK(paramBoard.IsValid(0,0,9) == true);
+        CHECK(paramBoard.IsValid(0,0,5) == false);
+        CHECK(paramBoard.IsValid(0,0,8) == false);
+        CHECK(paramBoard.IsValid(0,0,3) == false);
 
-        CHECK(defaultBoard.IsValid(4,1,3) == true);
-        CHECK(defaultBoard.IsValid(4,1,8) == false); // fails subgrid check
-        CHECK(defaultBoard.IsValid(4,1,7) == false); // fails row check
-        CHECK(defaultBoard.IsValid(4,1,5) == false); // fails column check
+        CHECK(paramBoard.IsValid(4,1,3) == true);
+        CHECK(paramBoard.IsValid(4,1,8) == false); // fails subgrid check
+        CHECK(paramBoard.IsValid(4,1,7) == false); // fails row check
+        CHECK(paramBoard.IsValid(4,1,5) == false); // fails column check
 
-        CHECK(defaultBoard.IsValid(6,4,1) == false); // fails column check
-        CHECK(defaultBoard.IsValid(6,4,2) == false); // fails subgrid and col check
-        CHECK(defaultBoard.IsValid(6,4,6) == false); // fails row check 
-        CHECK(defaultBoard.IsValid(6,4,3) == true);
+        CHECK(paramBoard.IsValid(6,4,1) == false); // fails column check
+        CHECK(paramBoard.IsValid(6,4,2) == false); // fails subgrid and col check
+        CHECK(paramBoard.IsValid(6,4,6) == false); // fails row check 
+        CHECK(paramBoard.IsValid(6,4,3) == true);
     }
-    defaultBoard.Print();
-    CHECK(defaultBoard.Solve() == true);
-    defaultBoard.Print();
+    paramBoard.Print();
+    CHECK(paramBoard.Solve() == true);
+    paramBoard.Print();
     
 }
